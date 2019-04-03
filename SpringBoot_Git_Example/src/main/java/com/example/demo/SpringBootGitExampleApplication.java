5c2300f10f05856943fc747264228b6acf968284
Swarit <Swarit@Dell>
2019-04-03 23:53:43 +0530

    Commit and Push

---
+++ SpringBoot_Git_Example/src/main/java/com/example/demo/SpringBootGitExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitExampleApplication.class, args);
+		
+		System.out.println("Hey Are you Remember me?");
+	}
+
+}
